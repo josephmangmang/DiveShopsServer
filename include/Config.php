38517df195260f65e3d268a650eb583d797a8e24
Joseph Mangmang <joseph.deplacer@gmail.com>
2017-03-10 19:10:34 +0800

    change database name

--- include/Config.php
+++ include/Config.php
@@ -5,7 +5,7 @@
  * Created 15-Feb-2017
  *
  */
-define('DB_NAME', 'divetym');
+define('DB_NAME', 'dive_tym_database');
 define('DB_USERNAME', 'skimfix');
 define('DB_PASSWORD', 'wkjW391rDY');
 define('DB_HOST', 'localhost');
