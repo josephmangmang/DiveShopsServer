f77912b03c38a0e9e907d0617a6fdf61364de5f7
Joseph Mangmang <joseph.deplacer@gmail.com>
2017-03-10 18:33:18 +0800

    redirect user to 404.php if user access unknown url

---
+++ public_html/error/404.php
@@ -0,0 +1,9 @@
+<?php
+
+/* 
+ * Copyright 2017 Joseph Mangmang.
+ * Created 10-Mar-2017
+ *
+ */
+
+echo json_encode(array('error' => true, 'message' =>'Not found'));
---
+++ public_html/index.php
@@ -0,0 +1,8 @@
+<?php
+
+/* 
+ * Copyright 2017 Joseph Mangmang.
+ * Created 10-Mar-2017
+ *
+ */
+
